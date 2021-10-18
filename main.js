86266035b539869c1d6245df038773b58b28ed16
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-10-19 00:16:50 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-let fruits = ['Apple', 'Banana', 'Cherry'];
-console.log(fruits[1]);
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function() {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
