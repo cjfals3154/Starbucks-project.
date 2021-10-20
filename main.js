99b91e27b4accdde6e321e7346c3df84e9665fa6
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-10-21 00:00:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window. addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if (window.scrollY > 500){
+    //배지 숨기기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else{
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
