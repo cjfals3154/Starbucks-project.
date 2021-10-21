495a8e45daf8550fbb57f6d63ce68d29a05fba1f
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-10-22 00:41:15 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ window. addEventListener('scroll', _.throttle(function(){
   console.log(window.scrollY);
   if (window.scrollY > 500){
     //배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션)
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
@@ -34,3 +35,13 @@ window. addEventListener('scroll', _.throttle(function(){
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+const fadeEls=document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index){
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(fadeEl, 1,{
+      delay:(index+1) * .7, //0.7 1.4 2.1 2.8
+      opacity:1
+    });
+});
