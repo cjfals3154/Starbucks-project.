75581ecb056ccb4e9fd75a2ad70ebc7b08ea6716
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-10-25 17:47:30 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -51,5 +51,22 @@ new Swiper('.notice-line .swiper-container', {
   direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
-  
+  });
+
+  new Swiper('.promotion .swiper-container',{
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 4000
+  },
+  pagination:{
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true //사용자의 페이지 번호 요소 제어여부 
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
   });
