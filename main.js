06b296ae5f4121aa8acd02488b74101b8d954a54
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-10-31 23:40:03 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -84,3 +84,28 @@ promotionToggleBtn.addEventListener('click',function (){
     promotionEl.classList.remove('hide');
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size){
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(selector, //선택자
+     random(1.5, 2.5), //애니메이션 동작 시간
+    { //옵션
+      y: size, //y축으로 얼마만큼 움직이면서 움질일 것인가.
+      repeat: -1, //얼마나 반복할 것이냐 (무한)
+      yoyo: true, //아래로 갔다가 다시 위로 올라갈 것 
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+      
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
