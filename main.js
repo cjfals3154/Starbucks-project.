77f25fed63b20757e807804a03df28f0bb698a5b
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2021-11-02 01:10:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -70,6 +70,17 @@ new Swiper('.notice-line .swiper-container', {
     nextEl: '.promotion .swiper-next'
   }
   });
+  new Swiper('.awards .swiper-container',{
+    autoplay:true,
+    loop:true,
+    spaceBetween:30,
+    slidesPerView:5,
+    navigation:{
+      prevEl:'.awards .swiper-prev',
+      nextEl:'.awards .swiper-next',
+    }
+  });
+
 
   const promotionEl = document.querySelector('.promotion');
   const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -109,3 +120,18 @@ function floatingObject(selector, delay, size){
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl){
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
